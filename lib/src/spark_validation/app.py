31cd67eb9b245306cf8f18e07c0e4394267765c6
utsavjha14@gmail.com <utsav.jha@ing.com>
2020-04-22 12:19:53 +0200

    Resolved MR and integrated FE

--- lib/src/spark_validation/app.py
+++ lib/src/spark_validation/app.py
@@ -5,23 +5,28 @@ from flask import Flask, jsonify, request, make_response
 
 from spark_validation.dataframe_validation import file_system_validator
 
-application = Flask(__name__)
+application = Flask(__name__, static_url_path="")
 
 
 @application.route("/")
 def index():
-    return "Data Validation Framework"
+    message = "Hello, World"
+    return application.send_static_file('index.html')
+    # return render_template('./static/index.html', message=message)
 
 
-@application.route("/validate", methods=["POST"])
+@application.route("/api/validate", methods=["POST"])
 def validate():
     json_input = request.get_json(force=True)
     with open('config.json', 'w') as fp:
         json.dump(json_input, fp)
     sys.argv = ["example.py", "-c", 'config.json']
 
+    print("JSON: {}".format(json_input))
+
     file_system_validator.init()
     response = {"validation": "yes"}
+
     return make_response(jsonify(response), 200)
 
 
